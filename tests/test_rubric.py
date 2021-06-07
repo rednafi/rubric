a97712361ddf7fa19c9f4bc5b35c7084b2c3abf3
rednafi <redowan.nafi@gmail.com>
2021-06-07 19:55:02 +0600

    All tests are now passing

--- tests/test_rubric.py
+++ tests/test_rubric.py
@@ -23,34 +23,34 @@ async def test_create_file(tmp_path):
         await rubric.copy_over("myp.ini", dirname=str(d))
 
 
-# @pytest.mark.asyncio
-# async def test_consumer(tmp_path):
-#     d = tmp_path / "dest"
-#     d.mkdir()
+@pytest.mark.asyncio
+async def test_consumer(tmp_path):
+    d = tmp_path / "dest"
+    d.mkdir()
 
-#     p = d / "pyproject.toml"
-#     q = d / "README.md"
-#     r = d / "requirements-dev.in"
+    p = d / "pyproject.toml"
+    q = d / "README.md"
+    r = d / "requirements-dev.in"
 
-#     # This should copy all the files from rubric/ to the temporary directory.
-#     await rubric.consumer(dirname=str(d), overwrite=True)
+    # This should copy all the files from rubric/ to the temporary directory.
+    await rubric.consumer(dirname=str(d), overwrite=True)
 
-#     # Check whether there are all the files in the directory.
-#     assert len(list(d.iterdir())) == len(rubric.FILENAMES)
+    # Check whether there are all the files in the directory.
+    assert len(list(d.iterdir())) == len(rubric.FILENAMES)
 
-#     # Assert file contents.
-#     assert "tool.isort" in p.read_text()
-#     assert "center" in q.read_text()
-#     assert "pip-tools" in r.read_text()
+    # Assert file contents.
+    assert "tool.isort" in p.read_text()
+    assert "center" in q.read_text()
+    assert "pip-tools" in r.read_text()
 
 
-# def test_cli_entrypoint(tmp_path, capsys):
-#     d = tmp_path / "dest"
-#     d.mkdir()
+def test_cli_entrypoint(tmp_path, capsys):
+    d = tmp_path / "dest"
+    d.mkdir()
 
-#     rubric.cli_entrypoint(["run", f"--dirname={str(d)}"])
-#     capture = capsys.readouterr()
-#     out = capture.out
+    rubric.cli_entrypoint(["run", f"--dirname={str(d)}"])
+    capture = capsys.readouterr()
+    out = capture.out
 
-#     assert r"____(_)___" in out
-#     assert r"creating" or "exists" in out
+    assert r"____(_)___" in out
+    assert r"creating" or "exists" in out
